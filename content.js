8382d65b15b1581eadd674153cf9042c4ff169ff
Parker Kimbell <parker.kimbell@intel.com>
2016-05-17 22:12:13 -0700

    Scale font increase appropriately. > Add function for detecting if jquery object exists in array of jquery objects > Bug: functions are not being checked appropriately for being the array of already zoomed objects, and are zooming repeatedly

--- content.js
+++ content.js
@@ -5,48 +5,89 @@ var height_increase = .25;
 
 var font_increase = .25;
 
-var square = 1;
+var square = 5;
 
 var currentlyZoomed = [];
 
 $(window).mousemove(function(event) {
-	var msg = "Handler for .mousemove() called at ";
+/*	var msg = "Handler for .mousemove() called at ";
   	msg += event.pageX + ", " + event.pageY;
 	console.log(msg); 
-	currentlyZoomed = [];
-	var newly_zoomed = findNewZoomedElements(event);
-	zoomNewElements();
-	addNewZoomedElements();
+*/	currentlyZoomed = [];
+	var newlyZoomed = findNewZoomedElements(event);
+	console.log('newly zoomed')
+	console.dir(newlyZoomed)
+	zoomNewElements(newlyZoomed);
+	addNewZoomedElements(newlyZoomed);
 	restoreOldZoomedElements();
 	removeOldZoomedElements();
 });
 
 function findNewZoomedElements(event) {
-	getElementsFromQuadrant(event, square, square); // Upper right quad
-	getElementsFromQuadrant(event, -square, square); // Upper left quad
-	getElementsFromQuadrant(event, -square, -square); // Lower left quad
-	getElementsFromQuadrant(event, square, -square); // Lower right quad
+	var newlyZoomed = [];
+	newlyZoomed = getElementsFromQuadrant(event, square, square, newlyZoomed); // Upper right quad
+	newlyZoomed = getElementsFromQuadrant(event, -square, square, newlyZoomed); // Upper left quad
+	newlyZoomed = getElementsFromQuadrant(event, -square, -square, newlyZoomed); // Lower left quad
+	newlyZoomed = getElementsFromQuadrant(event, square, -square, newlyZoomed); // Lower right quad
+	return newlyZoomed;
 }
 
-function getElementsFromQuadrant(event, xCoord, yCoord) {
-	for (var i = 0; i < xCoord; i++) {
-		for (var j = 0; j < yCoord; j++) {
+function getElementsFromQuadrant(event, xMax, yMax, newlyZoomed) {
+	for (var i = 0; i < xMax; i++) {
+		for (var j = 0; j < yMax; j++) {
 			var elem = $(document.elementFromPoint(event.pageX + i, event.pageY + j));
-			currentlyZoomed.push(elem);
+			if (jQueryObjectInArray(elem, newlyZoomed) === -1 && jQueryObjectInArray(elem, currentlyZoomed) === -1) { // Case: element is not currently zoomed
+				console.log('adding new item');
+				console.dir(elem);
+				newlyZoomed.push(elem);
+			}
 		}
 	}
+	return newlyZoomed;
 }
 
-function zoomNewElements() {
-	for (var i = 0; i < currentlyZoomed.length; i++) {
-		/*var parsedVal = parseInt($(currentlyZoomed[i].css('font-size')), 10);
-		console.log('Parsed val ' + parsedVal);*/
-		$(currentlyZoomed[i]).css('font-size', '35px');
+// Finds if a jQuery objects exists within an array of jQuery objects
+// returns index of object if it exists, 
+// else returns -1
+function jQueryObjectInArray($obj, arr) {
+	for (var i = 0; i < arr.length; i++) {
+		if ($obj.is(arr[i])) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+function zoomNewElements(newlyZoomed) {
+	var len = newlyZoomed.length;
+	for (var i = 0; i < len; i++) {
+		scaleFont(newlyZoomed[i]);	
 	}
 }
 
-function addNewZoomedElements() {
+// possible issue. Not sure if font size is always being pulled in as pixels, and always being set as pixels.
+// $.css pulls the computed value. Is this guaranteed to be in pixels in chrome?
+function scaleFont($element) {
+	var parsedVal = parseInt($element.css('font-size'), 10);
+	console.log('Parsed font ' + parsedVal);
+	$element.originalFontSize = parsedVal;
+	$element.css('font-size', parsedVal + parsedVal * font_increase);
+}
 
+function scaleWidth() {
+
+}
+
+function scaleHeight() {
+
+}
+
+function addNewZoomedElements(newlyZoomed) {
+	for (var i = 0; i < newlyZoomed.length; i++) {
+		currentlyZoomed.push(newlyZoomed[i]);
+	}
+	console.log('currentlyZoomed');
+	console.dir(currentlyZoomed);
 }
 
 function restoreOldZoomedElements() {
